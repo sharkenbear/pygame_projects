aef6ef9f3d446c4dd628c91dcbb0a1d94bbf42cf
Azure <link.lock2012@gmail.com>
2025-10-14 12:56:17 +0100

    update

--- sprite_map_maker.py
+++ sprite_map_maker.py
@@ -3,6 +3,7 @@ import random
 from icecream import ic
 
 import sprite_map_maker_essential
+import json_editor
 
 pygame.init()
 
@@ -24,11 +25,32 @@ BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
 WHITE = (255, 255, 255)
 
+TERQOISE = (0, 170, 170)
+ORANGE = (255, 135, 0)
+PINK = (255, 164, 255)
+YELLOW = (255, 255, 0)
+PURPLE = (220, 0, 220)
+CYAN = (0, 255, 255)
+
 SIDE_GRID_LENGTH = 8
 
 def save_map(full_colour_grid):
-    print("saved!")
-    print("(not actually i have not implemented that yet)")
+    json_editor.set_file(full_colour_grid)
+    print("saved to grid_file.json!")
+
+def draw_opacity_text(screen, x, y, text, colour, font_size, opacity = 255):
+    font = pygame.font.SysFont('arial', font_size)
+    text = font.render(str(text), True, (255,0,0))
+    surface = pygame.Surface(text.get_size(), pygame.SRCALPHA)
+    surface.blit(text, (0,0))
+    surface.fill((colour[0], colour[1], colour[2], opacity), None, pygame.BLEND_RGBA_MULT)
+    screen.blit(surface, (x, y))
+
+def draw_centered_text(screen, x, y, text, colour, font_size):
+    font = pygame.font.SysFont('arial', font_size)
+    text = font.render(str(text), True, colour)
+    text_rect = text.get_rect(center=(x, y))
+    screen.blit(text, text_rect)
 
 def get_grid_rects(grid, grid_length, deepness):
     for row in range(0, 8):
@@ -102,33 +124,45 @@ def draw_brushes_and_set_colours(screen, screen_width, brush_png, bucket_png, to
         bucket_colour = (136, 10, 232)
         brush_colour = (0, 0, 0)
 
-    # draws the boxes around the brush
+    # draws the boxes around the tools
     draw_rect(screen, 25, 475, (244, 234, 87), 65, 65)
     draw_rect(screen, 30, 480, brush_colour, 55, 55)
 
-    # draws the boxes around the bucket
     draw_rect(screen, 115, 475, (244, 234, 87), 65, 65)
     draw_rect(screen, 120, 480, bucket_colour, 55, 55)
 
     # draws the boxes of set colours
-    # draws the 
-    for row in range(20, 141, 60):
-        for column in range(575, 636, 60):
-            draw_rect(screen, row, column, (244, 234, 87), 45, 45)
-
+    for row in range(890, 1101, 100):
+        for column in range(275, 576, 100):
+            draw_rect(screen, row, column, (174, 174, 174), 80, 80)
+    
+    # terqoise: 25x 580y to 60x 615y
+    draw_rect(screen, 895, 480, (0, 170, 170), 70, 70)
+    # orange: 85x 580y to 110x 615y
+    draw_rect(screen, 995, 480, (255, 135, 0), 70, 70)
+    # pink: 145x 580y to 180x 615y
+    draw_rect(screen, 1095, 480, (255, 164, 255), 70, 70)
+    
     # red: 25x 580y to 60x 615y
-    draw_rect(screen, 25, 580, (255, 0, 0), 35, 35)
+    draw_rect(screen, 895, 280, (255, 0, 0), 70, 70)
     # green: 85x 580y to 110x 615y
-    draw_rect(screen, 85, 580, (0, 255, 0), 35, 35)
+    draw_rect(screen, 995, 280, (0, 255, 0), 70, 70)
     # blue: 145x 580y to 180x 615y
-    draw_rect(screen, 145, 580, (0, 0, 255), 35, 35)
+    draw_rect(screen, 1095, 280, (0, 0, 255), 70, 70)
+
+    # yellow: 25x 640y to 60x 675y
+    draw_rect(screen, 895, 380, (255, 255, 0), 70, 70)
+    # purple: 85x 640y to 110x 675y
+    draw_rect(screen, 995, 380, (220, 0, 220), 70, 70)
+    # cyan: 145x 640y to 180x 675y
+    draw_rect(screen, 1095, 380, (0, 255, 255), 70, 70)
 
     # black: 25x 640y to 60x 675y
-    draw_rect(screen, 25, 640, (0, 0, 0), 35, 35)
+    draw_rect(screen, 895, 580, (0, 0, 0), 70, 70)
     # white: 85x 640y to 110x 675y
-    draw_rect(screen, 85, 640, (255, 255, 255), 35, 35)
+    draw_rect(screen, 995, 580, (255, 255, 255), 70, 70)
     # grey: 145x 640y to 180x 675y
-    draw_rect(screen, 145, 640, (128, 128, 128), 35, 35)
+    draw_rect(screen, 1095, 580, (128, 128, 128), 70, 70)
 
 def draw_line(screen, startx, starty, endx, endy, colour = (0, 0, 0), width = 1):
     pygame.draw.line(screen, colour, (startx, starty), (endx, endy), width)
@@ -154,7 +188,7 @@ def type_msg(screen, font, x, y, text, colour):
 
 def main():
 
-    # foldable list of general variables (244, 234, 87)
+    # foldable list of general variables
     if True:
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
@@ -192,6 +226,11 @@ def main():
         tool_selected = BRUSH
         brush_png = None
         bucket_png = None
+        trying_delete = False
+
+        saved_text = False
+        opacity = 255
+        t = 255
 
         cmd = False
 
@@ -211,12 +250,21 @@ def main():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LMETA or event.key == pygame.K_RMETA:
                     cmd = True
+                
                 if cmd and event.key == pygame.K_s:
                     save_map(full_colour_grid)
+                    saved_text = True
+                
+                if cmd and event.key == pygame.K_BACKSPACE and trying_delete:
+                    full_colour_grid = [ [(0, 0, 0)] * full_grid_length for _ in range(0, full_grid_length)]
+                
+                if cmd and event.key == pygame.K_BACKSPACE:
+                    trying_delete = True
 
             elif event.type == pygame.KEYUP:
                 if event.key == pygame.K_LMETA or event.key == pygame.K_RMETA:
                     cmd = False
+                    trying_delete = False
 
             mouse_pos = pygame.mouse.get_pos()
             if event.type == pygame.MOUSEBUTTONDOWN or out_of_range:
@@ -258,34 +306,57 @@ def main():
                 elif (mouse_pos[0] >= 25 and mouse_pos[0] <= 180) and (mouse_pos[1] >= 330 and mouse_pos[1] <= 360):
                     slider_change = BLUE_COLOUR
 
+
                 # changes the colour to one of the set colours if they are pressed
-                # red: 25x 580y to 60x 615y
-                elif (mouse_pos[0] >= 25 and mouse_pos[0] <= 60) and (mouse_pos[1] >= 580 and mouse_pos[1] <= 615):
+                # red
+                elif (mouse_pos[0] >= 890 and mouse_pos[0] <= 970) and (mouse_pos[1] >= 275 and mouse_pos[1] <= 355):
                     current_colour = RED
                     set_colour_change = current_colour
-
-                # green: 85x 580y to 110x 615y
-                elif (mouse_pos[0] >= 85 and mouse_pos[0] <= 110) and (mouse_pos[1] >= 580 and mouse_pos[1] <= 615):
+                # green
+                elif (mouse_pos[0] >= 990 and mouse_pos[0] <= 1070) and (mouse_pos[1] >= 275 and mouse_pos[1] <= 355):
                     current_colour = GREEN
                     set_colour_change = current_colour
-
-                # blue: 145x 580y to 180x 615y
-                elif (mouse_pos[0] >= 145 and mouse_pos[0] <= 180) and (mouse_pos[1] >= 580 and mouse_pos[1] <= 615):
+                # blue
+                elif (mouse_pos[0] >= 1090 and mouse_pos[0] <= 1170) and (mouse_pos[1] >= 275 and mouse_pos[1] <= 355):
                     current_colour = BLUE
                     set_colour_change = current_colour
 
-                # black: 25x 640y to 60x 675y
-                elif (mouse_pos[0] >= 25 and mouse_pos[0] <= 60) and (mouse_pos[1] >= 640 and mouse_pos[1] <= 675):
-                    current_colour = BLACK
+                # terqoise
+                elif (mouse_pos[0] >= 890 and mouse_pos[0] <= 970) and (mouse_pos[1] >= 475 and mouse_pos[1] <= 555):
+                    current_colour = TERQOISE
+                    set_colour_change = current_colour
+                # orange
+                elif (mouse_pos[0] >= 990 and mouse_pos[0] <= 1070) and (mouse_pos[1] >= 475 and mouse_pos[1] <= 555):
+                    current_colour = ORANGE
+                    set_colour_change = current_colour
+                # pink
+                elif (mouse_pos[0] >= 1090 and mouse_pos[0] <= 1170) and (mouse_pos[1] >= 475 and mouse_pos[1] <= 555):
+                    current_colour = PINK
                     set_colour_change = current_colour
 
-                # white: 85x 640y to 110x 675y
-                elif (mouse_pos[0] >= 85 and mouse_pos[0] <= 110) and (mouse_pos[1] >= 640 and mouse_pos[1] <= 675):
+                # yellow
+                elif (mouse_pos[0] >= 890 and mouse_pos[0] <= 970) and (mouse_pos[1] >= 375 and mouse_pos[1] <= 455):
+                    current_colour = YELLOW
+                    set_colour_change = current_colour
+                # purple
+                elif (mouse_pos[0] >= 990 and mouse_pos[0] <= 1070) and (mouse_pos[1] >= 375 and mouse_pos[1] <= 455):
+                    current_colour = PURPLE
+                    set_colour_change = current_colour
+                # cyan
+                elif (mouse_pos[0] >= 1090 and mouse_pos[0] <= 1170) and (mouse_pos[1] >= 375 and mouse_pos[1] <= 455):
+                    current_colour = CYAN
+                    set_colour_change = current_colour
+
+                # black
+                elif (mouse_pos[0] >= 890 and mouse_pos[0] <= 970) and (mouse_pos[1] >= 575 and mouse_pos[1] <= 655):
+                    current_colour = BLACK
+                    set_colour_change = current_colour
+                # white
+                elif (mouse_pos[0] >= 990 and mouse_pos[0] <= 1070) and (mouse_pos[1] >= 575 and mouse_pos[1] <= 655):
                     current_colour = WHITE
                     set_colour_change = current_colour
-            
-                # grey: 145x 640y to 180x 675y
-                elif (mouse_pos[0] >= 145 and mouse_pos[0] <= 180) and (mouse_pos[1] >= 640 and mouse_pos[1] <= 675):
+                # grey
+                elif (mouse_pos[0] >= 1090 and mouse_pos[0] <= 1170) and (mouse_pos[1] >= 575 and mouse_pos[1] <= 655):
                     current_colour = GREY
                     set_colour_change = current_colour
 
@@ -306,10 +377,28 @@ def main():
 
         screen.fill((45, 52, 92))
 
-                                    # --display from here onward!--
-
         if mouse_pos == None:
             mouse_pos = pygame.mouse.get_pos()
+
+                                                # --display from here onward!--
+
+        if saved_text:
+            t = t - 0.75
+
+            if t < 128:
+                opacity = int(t) + 55
+            
+            if t == -30:
+                saved_text = False
+                t = 255
+            draw_opacity_text(screen, 900, 20, "saved!", (0, 0, 0), 40, opacity)
+            
+
+        if trying_delete and not saved_text:
+            draw_centered_text(screen, 90, 560, "press command +", (0, 0, 0), 20)
+            draw_centered_text(screen, 110, 580, "delete again to confirm", (0, 0, 0), 20)
+            draw_centered_text(screen, 90, 600, "delete or let go of", (0, 0, 0), 20)
+            draw_centered_text(screen, 95, 620, "command to cancel", (0, 0, 0), 20)
 
         # draws the currently selected colour in a box below the side grid
         draw_rect(screen, 75, 370, (244, 234, 87), 60, 60)
@@ -318,12 +407,9 @@ def main():
         # draws the colour bars
         slider_red_pos, slider_green_pos, slider_blue_pos, current_colour = draw_colour_bar(slider_red_pos, slider_green_pos, slider_blue_pos, current_colour, mouse_pos, slider_change, screen, set_colour_change)
 
-        # draws the main grid
+        # draws the grids
         sprite_map_maker_essential.main(colour_side_grid, current_colour, full_colour_grid, full_grid_length, screen, selected, SIDE_GRID_LENGTH)
-        # (selected, full_grid_length * 10, 215, screen, 15, full_colour_grid)
 
-        # draws the grid on the side of the screen
-        # display_side_grid(colour_side_grid, SIDE_GRID_LENGTH * 2.5, 25, screen, 25)
         draw_brushes_and_set_colours(screen, SCREEN_WIDTH, brush_png, bucket_png, tool_selected)
 
         pygame.display.update()
